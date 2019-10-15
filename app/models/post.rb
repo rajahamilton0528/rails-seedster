3d023a2b29d866d523dc92fc3438bc8cab7cf082
rajahamilton0528 <rajahamilton0528@gmail.com>
2019-10-14 22:42:30 -0500

    Add scopes
    Add method to merge multiple scopes,
    which could be used when the scopes
    are conditionally set (otherwise they could
    just be chained)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
+
   has_many :comments
+
+  scope :with_comments, -> { joins(:comments) }
+  scope :by_author, -> (user) { where(user: user) }
+
+  def self.apply_scopes(*filters)
+    filters.inject(all) do |scope_chain, filter|
+      scope_chain.merge(filter)
+    end
+  end
 end
